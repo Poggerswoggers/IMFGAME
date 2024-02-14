d30839224abea8fe964f65e11af1f7eb9bed54b5
Poggerswoggers <jasonsohhuihong@gmail.com>
2024-02-14 22:47:49 +0800

    WIRE INNIT

--- IMFBOMBOMBDEFUSE/Assets/Scripts/CFOUR.cs
+++ IMFBOMBOMBDEFUSE/Assets/Scripts/CFOUR.cs
@@ -6,17 +6,54 @@ using TMPro;
 
 public class CFOUR : MonoBehaviour
 {
+    //Timer
     [SerializeField] float maxTime;
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] AudioSource bombSound;
     public float currentTime;
+    //
+    public enum WireCombos { R2G1B1 , R1G1B1Y1 , G1B2Y1 }
+    public WireCombos wireCombo; 
+
+
+
+    public List<wire> wires;
+
 
     private void Start()
     {
         currentTime = maxTime;
         bombSound.enabled = true;
+
+        InitialiseWireCombo();
     }
 
+    void InitialiseWireCombo()
+    {        
+
+        switch(wireCombo)
+        {
+            case WireCombos.R2G1B1:
+                SetWireColor(wires[0], "Red" );
+                SetWireColor(wires[1], "Red");
+                SetWireColor(wires[2], "Green");
+                SetWireColor(wires[3], "Blue");
+                break;
+
+            case WireCombos.R1G1B1Y1:
+                SetWireColor(wires[0], "Red");
+                SetWireColor(wires[1], "Blue");
+                SetWireColor(wires[2], "Blue");
+                SetWireColor(wires[3], "Yellow");
+                break;
+
+        }
+    }
+
+    void SetWireColor(wire currentWire, string color)
+    {
+        currentWire.SetWireColor(color);
+    }
 
     private void OnMouseDown()
     {
--- IMFBOMBOMBDEFUSE/Assets/Scripts/wire.cs
+++ IMFBOMBOMBDEFUSE/Assets/Scripts/wire.cs
@@ -1,19 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
+
 
 public class wire : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public MeshRenderer mr;
+    public enum wireColor
     {
-        
+        None,
+        Red,
+        Blue,
+        Yellow,
+        Green
     }
+    public wireColor thisWireColor = wireColor.None;
 
-    // Update is called once per frame
-    void Update()
+
+
+    public void SetWireColor(string color)
     {
-        
+        thisWireColor = (wireColor)Enum.Parse(typeof(wireColor), color);
+
+        switch(thisWireColor)
+        {
+            case wireColor.Red:
+                mr.material.color = Color.red;
+                break;
+
+            case wireColor.Blue:
+                mr.material.color = Color.blue;
+                break;
+
+            case wireColor.Yellow:
+                mr.material.color = Color.yellow;
+                break;
+
+            case wireColor.Green:
+                mr.material.color = Color.green;
+                break;
+                
+        }
     }
 
     private void OnMouseOver()
